4a8db3be70b6605756fbb297654cc8102bf57420
Jenisha <jenishereyl@gmail.com>
2026-01-06 07:17:34 +0530

    Add script.js

---
+++ day5/script.js
@@ -0,0 +1,30 @@
+for(let i = 1; i <= 50; i++){
+    if( i % 2 === 0){
+        console.log("even");
+    }else{
+        console.log(i);
+    }
+}
+
+// An array of numbers and a function that returns only even numbers using filter
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function getEvenNumbers(arr) {
+  return arr.filter(num => num % 2 === 0);
+}
+console.log(getEvenNumbers(numbers));
+
+// An object student with name, age, and skills (array)
+const student = {
+  name: "Jenisha",
+  age: 19,
+  skills: ["HTML", "CSS", "JavaScript"]
+};
+
+// Add a new skill to the object
+student.skills.push("React");
+console.log(student.skills);
+
+// Use a loop to print all keys and values of the object
+for (let key in student) {
+  console.log(key + ":", student[key]);
+}
