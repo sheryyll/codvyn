6af80d1c55cb28d531a2820937aff0c739569353
Jenisha <jenishereyl@gmail.com>
2026-01-13 17:30:13 +0530

    Add Day 9 React Basics Part 2

---
+++ day9/app2/src/App.jsx
@@ -0,0 +1,24 @@
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <>
+      <Card
+        title="React Components"
+        description="Components help break the UI into reusable pieces."
+      />
+
+      <Card
+        title="Props"
+        description="Props allow data to be passed from parent to child components."
+      />
+
+      <Card
+        title="Reusability"
+        description="Using props makes components flexible and reusable."
+      />
+    </>
+  );
+}
+
+export default App;
