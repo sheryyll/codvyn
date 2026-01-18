9c84f01a69eadfb465b3380e58c93cf9185aefe5
Jenisha <jenishereyl@gmail.com>
2026-01-18 20:41:32 +0530

    Add Day 17 MongoDB & Mongoose Fundamentals Part 1

--- day15/index.js
+++ day15/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const usersRouter = require("./routes/users");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
 
+connectDB();
+
 app.use(express.json());
+
 
 app.get("/", (req, res) => {
   res.send("Day 15 Express server is running");
