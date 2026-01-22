ae973553f94db6f6235902e2679e27722382a3bd
Jenisha <jenishereyl@gmail.com>
2026-01-22 19:11:31 +0530

    Add Day 15 Node.js & Express Fundamentals Part 1

---
+++ day_15/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Day 15 Express server is running");
+});
+
+
+app.get("/api/hello", (req, res) => {
+  res.json({
+    message: "Hello from API"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
